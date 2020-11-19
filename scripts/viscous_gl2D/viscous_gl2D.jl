9c12d67ed5361dd0e8a09b20e41393e7adb37611
Ludovic Räss <ludovic.rass@gmail.com>
2020-11-19 13:53:26 +0100

    Updated indent

--- scripts/viscous_gl2D/viscous_gl2D.jl
+++ scripts/viscous_gl2D/viscous_gl2D.jl
@@ -14,7 +14,7 @@ using PyPlot, Statistics
 	dτv     = min(dx,dy)^2/μ/4.1/2 # timestep
 	dτp     = 4.1*μ/max(nx,ny)
 	(xc, yc ) = (LinRange(-lx/2, lx/2, nx), LinRange(-ly/2, ly/2, ny))
-    (Xc2,Yc2) = ([x for x=xc,y=yc], [y for x=xc,y=yc])
+	(Xc2,Yc2) = ([x for x=xc,y=yc], [y for x=xc,y=yc])
 	(X2r,Y2r) = (Xc2*cosd(α) - sind(α)*Yc2, Xc2*sind(α) + cosd(α)*Yc2)
 	# initial conditions
 	Pt      = zeros(Float64, nx  ,ny  )
@@ -28,9 +28,9 @@ using PyPlot, Statistics
 	τ_xy    = zeros(Float64, nx-1,ny-1) 
 	# action - iteration loop
 	for iτ = 1:niτ
-	    # pressure and stress
-	    ∇V    .= diff(Vx,dims=1)./dx .+ diff(Vy,dims=2)./dy      
-	    Pt    .= Pt .- ∇V.*dτp 
+		# pressure and stress
+		∇V    .= diff(Vx,dims=1)./dx .+ diff(Vy,dims=2)./dy      
+		Pt    .= Pt .- ∇V.*dτp 
 		σ_xx  .= 2.0*μ.*(diff(Vx,dims=1)./dx .- 1/3*∇V) .- Pt
 		σ_yy  .= 2.0*μ.*(diff(Vy,dims=2)./dy .- 1/3*∇V) .- Pt
 		τ_xy  .= μ.*(diff(Vx[2:end-1,:],dims=2)./dy .+ diff(Vy[:,2:end-1],dims=1)./dx)
@@ -54,7 +54,7 @@ using PyPlot, Statistics
 	(Xp,  Yp ) = (X2r[1:st:end,1:st:end], Y2r[1:st:end,1:st:end])
 	(Vxp, Vyp) = (0.5*(Vx[1:st:end-1,1:st:end  ]+Vx[2:st:end,1:st:end]), 0.5*(Vy[1:st:end  ,1:st:end-1]+Vy[1:st:end,2:st:end]))
 	subplot(311), pcolor(X2r,Y2r, 1e-3*Pt, shading="auto"), plt.axis("off"), plt.title("pressure [kPa]"), plt.colorbar(fraction=0.02, pad=0.03)
-	              quiver(Xp, Yp, Vxp, Vyp, pivot="mid", color="white")
+	quiver(Xp, Yp, Vxp, Vyp, pivot="mid", color="white")
 	subplot(312), pcolor(X2r,Y2r, s2d*Vx[2:end,:], shading="auto"), plt.axis("off"), plt.title("Vel-x [m/d]"), plt.colorbar(fraction=0.02, pad=0.03)
 	subplot(313), pcolor(X2r,Y2r, s2d*Vy[:,2:end], shading="auto"), plt.axis("off"), plt.title("Vel-y [m/d]"), plt.colorbar(fraction=0.02, pad=0.03)
 	return
--- scripts/viscous_gl2D/viscous_gl2D_gpu.jl
+++ scripts/viscous_gl2D/viscous_gl2D_gpu.jl
@@ -14,7 +14,7 @@ using CUDA, PyPlot, Statistics
 	dτv     = min(dx,dy)^2/μ/4.1/2 # timestep
 	dτp     = 4.1*μ/max(nx,ny)
 	(xc, yc ) = (LinRange(-lx/2, lx/2, nx), LinRange(-ly/2, ly/2, ny))
-    (Xc2,Yc2) = ([x for x=xc,y=yc], [y for x=xc,y=yc])
+	(Xc2,Yc2) = ([x for x=xc,y=yc], [y for x=xc,y=yc])
 	(X2r,Y2r) = (Xc2*cosd(α) - sind(α)*Yc2, Xc2*sind(α) + cosd(α)*Yc2)
 	# initial conditions
 	Pt      = CUDA.zeros(Float64, nx  ,ny  )
@@ -54,7 +54,7 @@ using CUDA, PyPlot, Statistics
 	(Xp,  Yp ) = (X2r[1:st:end,1:st:end], Y2r[1:st:end,1:st:end])
 	(Vxp, Vyp) = (0.5*(Vx[1:st:end-1,1:st:end  ]+Vx[2:st:end,1:st:end]), 0.5*(Vy[1:st:end  ,1:st:end-1]+Vy[1:st:end,2:st:end]))
 	subplot(311), pcolor(X2r,Y2r, 1e-3*Array(Pt), shading="auto"), plt.axis("off"), plt.title("pressure [kPa]"), plt.colorbar(fraction=0.02, pad=0.03)
-	                     quiver(Xp, Yp, Vxp, Vyp, pivot="mid", color="white")
+	quiver(Xp, Yp, Vxp, Vyp, pivot="mid", color="white")
 	subplot(312), pcolor(X2r,Y2r, s2d*Array(Vx[2:end,:]), shading="auto"), plt.axis("off"), plt.title("Vel-x [m/d]"), plt.colorbar(fraction=0.02, pad=0.03)
 	subplot(313), pcolor(X2r,Y2r, s2d*Array(Vy[:,2:end]), shading="auto"), plt.axis("off"), plt.title("Vel-y [m/d]"), plt.colorbar(fraction=0.02, pad=0.03)
 	return
